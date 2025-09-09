56107d856b7289d9a1428562bafcbbaec20eb829
Nexaplafrom <nexaplaform@gmail.com>
2025-09-09 23:15:20 +0200

    Creando crud de usuarios

--- src/main/java/com/fiverr/app/mic/userservices/application/security/cors/CorsRegistryConfiguration.java
+++ src/main/java/com/fiverr/app/mic/userservices/application/security/cors/CorsRegistryConfiguration.java
@@ -11,7 +11,9 @@ public class CorsRegistryConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://mic-userservices-production.up.railway.app")
                 .allowedMethods(HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
