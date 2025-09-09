6fbfede55b42f12030db3deeb509b7544963a0f2
Nexaplafrom <nexaplaform@gmail.com>
2025-09-09 18:19:46 +0200

    Creacion de crud de roles

--- src/main/java/com/fiverr/app/mic/userservices/application/security/cors/CorsRegistryConfiguration.java
+++ src/main/java/com/fiverr/app/mic/userservices/application/security/cors/CorsRegistryConfiguration.java
@@ -11,7 +11,7 @@ public class CorsRegistryConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:8080")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods(HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
