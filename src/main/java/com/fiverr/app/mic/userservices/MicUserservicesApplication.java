215c2aee03998f312f1fa6a96e5027e899d7dbe8
Nexaplafrom <nexaplaform@gmail.com>
2025-09-09 18:14:35 +0200

    Creacion de crud de roles

--- src/main/java/com/fiverr/app/mic/userservices/MicUserservicesApplication.java
+++ src/main/java/com/fiverr/app/mic/userservices/MicUserservicesApplication.java
@@ -1,9 +1,15 @@
 package com.fiverr.app.mic.userservices;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+        @Server(url = "/", description = "Default Server"),
+        @Server(url = "https://mic-userservices-production.up.railway.app/", description = "Production Server")
+})
 public class MicUserservicesApplication {
 
 	public static void main(String[] args) {
